96237295c8e00e0ea7b03aad453c945f9cbab359
jojodee <joan@projectcodex.co>
2016-03-10 12:09:00 +0200

    Updated my files to highlight matching numbers

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ function getRandomIntInclusive(min, max) {
 document.getElementById("div1").innerHTML = getRandomIntInclusive(1, 9);
 document.getElementById("div2").innerHTML = getRandomIntInclusive(1, 9);
 document.getElementById("div3").innerHTML = getRandomIntInclusive(1, 9);
+
+//class highlight{
+
+
+
+// document.getElementById("div2").classList.add("highlight");
+// document.getElementById("div3").classList.add("highlight");
+
+
+function highlightMatchingNumbers(){
+  if(document.getElementById("div1").innerHTML  === document.getElementById("div2").innerHTML  )
+  {C = "Numbers Matched = 2" + " <br> <br> " +  "Matching Number =" + " " + document.getElementById("div1").innerHTML ;
+    document.getElementById("div1").classList.add("highlight");
+    document.getElementById("div2").classList.add("highlight");}
+  else if(document.getElementById("div1").innerHTML  === document.getElementById("div3").innerHTML)
+  {document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " +  "Matching Number =" + " " + document.getElementById("div1").innerHTML
+    document.getElementById("div1").classList.add("highlight");
+    document.getElementById("div3").classList.add("highlight");
+  }else if(document.getElementById("div2").innerHTML  === document.getElementById("div3").innerHTML)
+  {document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " +  "Matching Number =" + " " + document.getElementById("div2").innerHTML
+    document.getElementById("div2").classList.add("highlight");
+    document.getElementById("div3").classList.add("highlight");
+  } if(document.getElementById("div1").innerHTML  === document.getElementById("div2").innerHTML){
+      if(document.getElementById("div2").innerHTML === document.getElementById("div3").innerHTML){
+        document.getElementById("message").innerHTML = "Numbers Matched = 3" + " <br> <br> " +  "Matching Number =" + " " + document.getElementById("div1").innerHTML
+        document.getElementById("div1").classList.add("highlight");
+        document.getElementById("div2").classList.add("highlight");
+        document.getElementById("div3").classList.add("highlight");}
+      else {
+          document.getElementById("nomatch").innerHTML
+      }
+  }
+}
+
+  highlightMatchingNumbers();
