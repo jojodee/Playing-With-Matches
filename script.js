0542dd3f92bd35c44eeeb8366499e3c04e077538
jojodee <joan@projectcodex.co>
2016-03-15 07:53:15 +0200

    corrected error in script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function getRandomIntInclusive(min, max) {
 }
 
 
-
 function createRandomNumbers(){
   document.getElementById("div1").innerHTML = getRandomIntInclusive(1, 9);
   document.getElementById("div2").innerHTML = getRandomIntInclusive(1, 9);
@@ -29,17 +28,17 @@ function unHighLight(){
   document.getElementById("div9").classList.remove("highlight");
 }
 
+
 function highlightMatchingNumbers() {
   if (document.getElementById("div1").innerHTML === document.getElementById("div2").innerHTML) {
 
-    document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div1").innerHTML;
+    document.getElementById("message").innerHTML =  "Row 1 matched" + "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div1").innerHTML;
+    // document.getElementById("message").innerHTML =  "Row 1 matched";
     document.getElementById("div1").classList.add("highlight");
     document.getElementById("div2").classList.add("highlight");
-  }
 
-function unHighlightdiv5(){
-    document.getElementById("div5").classList.remove("highlight");
-}
+
+  }
 
   if (document.getElementById("div1").innerHTML === document.getElementById("div3").innerHTML) {
     document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div1").innerHTML
@@ -107,34 +106,17 @@ if (document.getElementById("div1").innerHTML === document.getElementById("div7"
   document.getElementById("div7").classList.add("highlight");
 }
 
-if (document.getElementById("div1").innerHTML === document.getElementById("div8").innerHTML) {
-  document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div1").innerHTML;
-  document.getElementById("div1").classList.add("highlight");
-  document.getElementById("div8").classList.add("highlight");
-}
-
 if (document.getElementById("div1").innerHTML === document.getElementById("div9").innerHTML) {
   document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div1").innerHTML;
   document.getElementById("div1").classList.add("highlight");
   document.getElementById("div9").classList.add("highlight");
 }
 
-if (document.getElementById("div2").innerHTML === document.getElementById("div4").innerHTML) {
-  document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div2").innerHTML;
-  document.getElementById("div2").classList.add("highlight");
-  document.getElementById("div4").classList.add("highlight");
-}
 
 if (document.getElementById("div2").innerHTML === document.getElementById("div5").innerHTML) {
   document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div2").innerHTML;
   document.getElementById("div2").classList.add("highlight");
   document.getElementById("div5").classList.add("highlight");
-}
-
-if (document.getElementById("div2").innerHTML === document.getElementById("div6").innerHTML) {
-  document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div2").innerHTML;
-  document.getElementById("div2").classList.add("highlight");
-  document.getElementById("div6").classList.add("highlight");
 }
 
 if (document.getElementById("div2").innerHTML === document.getElementById("div8").innerHTML) {
@@ -146,7 +128,7 @@ if (document.getElementById("div2").innerHTML === document.getElementById("div8"
 if (document.getElementById("div3").innerHTML === document.getElementById("div9").innerHTML) {
   document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div3").innerHTML;
   document.getElementById("div3").classList.add("highlight");
-  document.getElementById("div5").classList.add("highlight");
+  document.getElementById("div9").classList.add("highlight");
 }
 
 if (document.getElementById("div3").innerHTML === document.getElementById("div5").innerHTML) {
@@ -179,14 +161,9 @@ if (document.getElementById("div4").innerHTML === document.getElementById("div7"
   document.getElementById("div7").classList.add("highlight");
 }
 
-if (document.getElementById("div4").innerHTML === document.getElementById("div8").innerHTML) {
-  document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div4").innerHTML;
-  document.getElementById("div4").classList.add("highlight");
-  document.getElementById("div8").classList.add("highlight");
-}
 
 if (document.getElementById("div5").innerHTML === document.getElementById("div7").innerHTML) {
-  document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div5").innerHTML;
+  document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div7").innerHTML;
   document.getElementById("div5").classList.add("highlight");
   document.getElementById("div7").classList.add("highlight");
 }
@@ -204,23 +181,19 @@ if (document.getElementById("div5").innerHTML === document.getElementById("div9"
 }
 
 
-if (document.getElementById("div6").innerHTML === document.getElementById("div8").innerHTML) {
-  document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div6").innerHTML;
-  document.getElementById("div6").classList.add("highlight");
-  document.getElementById("div8").classList.add("highlight");
-}
-
 if (document.getElementById("div6").innerHTML === document.getElementById("div9").innerHTML) {
   document.getElementById("message").innerHTML = "Numbers Matched = 2" + " <br> <br> " + "Matching Number =" + " " + document.getElementById("div6").innerHTML;
   document.getElementById("div6").classList.add("highlight");
   document.getElementById("div9").classList.add("highlight");
 }
 
+
 }
+
+
 
 var btn = document.getElementById("button");
 btn.addEventListener("click", function(){
-  unHighlightdiv5();
   unHighLight();
   createRandomNumbers();
   highlightMatchingNumbers();
